c218f0aaa23535bd53ee61be6d24147f1c9ddea8
Adster94 <adam.moorey@hotmail.co.uk>
2016-11-29 10:46:36 +0000

    Uploaded basic project file, with NES Controller (4-player) correctly set up.

---
+++ NESInputTest/Assets/movement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetButtonDown("A(NES)"))
+        {
+            this.transform.position += new Vector3(0.0f, 1.0f, 0.0f);
+        }
+        else if (Input.GetButtonDown("B(NES)"))
+        {
+            this.transform.position += new Vector3(1.0f, 0.0f, 0.0f);
+        }
+        else if (Input.GetButtonDown("Select(NES)"))
+        {
+            this.transform.position += new Vector3(0.0f, -1.0f, 0.0f);
+        }
+        else if (Input.GetButtonDown("Start(NES)"))
+        {
+            this.transform.position += new Vector3(-1.0f, 0.0f, 0.0f);
+        }
+
+        transform.position += new Vector3(Input.GetAxis("Horizontal(NES)") * 0.1f, Input.GetAxis("Vertical(NES)") * -0.1f, 0.0f);
+	}
+}
