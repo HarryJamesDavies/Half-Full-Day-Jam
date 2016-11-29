ae90a34143f9bec740b2522bc3fb67b0fceb8b7d
Adster94 <adam.moorey@hotmail.co.uk>
2016-11-29 11:42:05 +0000

    Updated the movement script. Four controllers should work independently now. Alongside, a more streamlined system for button management.

--- NESInputTest/Assets/movement.cs
+++ NESInputTest/Assets/movement.cs
@@ -1,103 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class movement : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class Movement : MonoBehaviour {
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         switch (gameObject.tag)
         {
             case "Player1":
-                if (Input.GetButtonDown("P1-A(NES)"))
-                {
-                    transform.position += new Vector3(0.0f, 1.0f, 0.0f);
-                    Debug.Log("Player 1 Press A");
-                }
-                else if (Input.GetButtonDown("P1-B(NES)"))
-                {
-                    transform.position += new Vector3(1.0f, 0.0f, 0.0f);
-                    Debug.Log("Player 1 Press B");
-                }
-                else if (Input.GetButtonDown("P1-Select(NES)"))
-                {
-                    transform.position += new Vector3(0.0f, -1.0f, 0.0f);
-                }
-                else if (Input.GetButtonDown("P1-Start(NES)"))
-                {
-                    transform.position += new Vector3(-1.0f, 0.0f, 0.0f);
-                }
-
-                transform.position += new Vector3(Input.GetAxis("P1-Horizontal(NES)") * 0.1f, Input.GetAxis("P1-Vertical(NES)") * -0.1f, 0.0f);
+                PlayerMovement(1);
                 break;
             case "Player2":
-                if (Input.GetButtonDown("P2-A(NES)"))
-                {
-                    transform.position += new Vector3(0.0f, 1.0f, 0.0f);
-                    Debug.Log("Player 2 Press A");
-                }
-                else if (Input.GetButtonDown("P2-B(NES)"))
-                {
-                    transform.position += new Vector3(1.0f, 0.0f, 0.0f);
-                    Debug.Log("Player 2 Press B");
-                }
-                else if (Input.GetButtonDown("P2-Select(NES)"))
-                {
-                    transform.position += new Vector3(0.0f, -1.0f, 0.0f);
-                }
-                else if (Input.GetButtonDown("P2-Start(NES)"))
-                {
-                    transform.position += new Vector3(-1.0f, 0.0f, 0.0f);
-                }
-
-                transform.position += new Vector3(Input.GetAxis("P2-Horizontal(NES)") * 0.1f, Input.GetAxis("P2-Vertical(NES)") * -0.1f, 0.0f);
+                PlayerMovement(2);
                 break;
             case "Player3":
-                if (Input.GetButtonDown("P3-A(NES)"))
-                {
-                    transform.position += new Vector3(0.0f, 1.0f, 0.0f);
-                }
-                else if (Input.GetButtonDown("P3-B(NES)"))
-                {
-                    transform.position += new Vector3(1.0f, 0.0f, 0.0f);
-                }
-                else if (Input.GetButtonDown("P3-Select(NES)"))
-                {
-                    transform.position += new Vector3(0.0f, -1.0f, 0.0f);
-                }
-                else if (Input.GetButtonDown("P3-Start(NES)"))
-                {
-                    transform.position += new Vector3(-1.0f, 0.0f, 0.0f);
-                }
-
-                transform.position += new Vector3(Input.GetAxis("P3-Horizontal(NES)") * 0.1f, Input.GetAxis("P3-Vertical(NES)") * -0.1f, 0.0f);
+                PlayerMovement(3);
                 break;
             case "Player4":
-                if (Input.GetButtonDown("P4-A(NES)"))
-                {
-                    transform.position += new Vector3(0.0f, 1.0f, 0.0f);
-                }
-                else if (Input.GetButtonDown("P4-B(NES)"))
-                {
-                    transform.position += new Vector3(1.0f, 0.0f, 0.0f);
-                }
-                else if (Input.GetButtonDown("P4-Select(NES)"))
-                {
-                    transform.position += new Vector3(0.0f, -1.0f, 0.0f);
-                }
-                else if (Input.GetButtonDown("P4-Start(NES)"))
-                {
-                    transform.position += new Vector3(-1.0f, 0.0f, 0.0f);
-                }
-
-                transform.position += new Vector3(Input.GetAxis("P4-Horizontal(NES)") * 0.1f, Input.GetAxis("P4-Vertical(NES)") * -0.1f, 0.0f);
+                PlayerMovement(4);
                 break;
             default:
                 break;
         }
 	}
+
+    void PlayerMovement(int _controller)
+    {
+        if (Input.GetButtonDown("P" + _controller + "-A(NES)"))
+        {
+            transform.position += new Vector3(0.0f, 1.0f, 0.0f);
+            Debug.Log("Player 1 Press A");
+        }
+        else if (Input.GetButtonDown("P" + _controller + "-B(NES)"))
+        {
+            transform.position += new Vector3(1.0f, 0.0f, 0.0f);
+            Debug.Log("Player 1 Press B");
+        }
+        else if (Input.GetButtonDown("P" + _controller + "-Select(NES)"))
+        {
+            transform.position += new Vector3(0.0f, -1.0f, 0.0f);
+        }
+        else if (Input.GetButtonDown("P" + _controller + "-Start(NES)"))
+        {
+            transform.position += new Vector3(-1.0f, 0.0f, 0.0f);
+        }
+
+        transform.position += new Vector3(Input.GetAxis("P" + _controller + "-Horizontal(NES)") * 0.1f, Input.GetAxis("P" + _controller + "-Vertical(NES)") * -0.1f, 0.0f);
+    }
 }
