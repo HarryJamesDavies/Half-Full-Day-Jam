f48119cd5175127aeb88be043db6f74402e478ea
Falsan <sam.britnell117@gmail.com>
2016-11-30 13:47:21 +0000

    The shooters now behave as they should with the states

--- NESInputTest/Assets/Scripts/ShooterManager.cs
+++ NESInputTest/Assets/Scripts/ShooterManager.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ShooterManager : MonoBehaviour {
+public class ShooterManager : MonoBehaviour
+{
 
     [SerializeField]
     GameObject sideShooterRef;
@@ -14,10 +15,51 @@ public class ShooterManager : MonoBehaviour {
     GameObject m_upShooter;
     [SerializeField]
     GameObject m_bottomShooter;
+    [SerializeField]
+    bool shootersInGame;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
+        shootersInGame = false;
+        
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        switch (StateManager.m_instance.m_currentState)
+        {
+            case StateManager.State.MENU:
+                {
+                    break;
+                }
+            case StateManager.State.PAUSE:
+                {
+                    break;
+                }
+            case StateManager.State.PLAY:
+                {
+                    if(shootersInGame == false)
+                    {
+                        SpawnShooters();
+                    }
+                    break;
+                }
+            case StateManager.State.GAMEOVER:
+                {
+                    if(shootersInGame == true)
+                    {
+                        DespawnShooters();
+                    }
+
+                    break;
+                }
+        }
+    }
+
+    void SpawnShooters()
+    {
         m_rightShooter = Instantiate(sideShooterRef);
         m_leftShooter = Instantiate(sideShooterRef);
         m_upShooter = Instantiate(sideShooterRef);
@@ -27,10 +69,17 @@ public class ShooterManager : MonoBehaviour {
         m_leftShooter.GetComponent<SideSpawnerScript>().SetSide(2);
         m_bottomShooter.GetComponent<SideSpawnerScript>().SetSide(1);
         m_upShooter.GetComponent<SideSpawnerScript>().SetSide(3);
+
+        shootersInGame = true;
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    void DespawnShooters()
+    {
+        Destroy(m_rightShooter);
+        Destroy(m_leftShooter);
+        Destroy(m_bottomShooter);
+        Destroy(m_upShooter);
+
+        shootersInGame = false;
+    }
 }
