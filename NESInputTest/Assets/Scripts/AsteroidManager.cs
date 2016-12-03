5d88633b729ea7e24ee59a225d722ce6627a76f8
HarryJamesDavies <daviesharry1@gmail.com>
2016-12-03 18:52:34 +0000

    Have added shooters to the corners so that players can't camp there

---
+++ NESInputTest/Assets/Scripts/AsteroidManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AsteroidManager : MonoBehaviour
+{
+    public GameObject m_asteroidSpawner;
+    public List<Vector3> m_spawnerPosition;
+    public float m_distance;
+    public List<GameObject> m_spawners;
+        
+	// Use this for initialization
+	void Start ()
+    {
+        for (int i = 0; i <= m_spawnerPosition.Count - 1; i++)
+        {
+            CreateSpawner(m_spawnerPosition[i]);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+
+    void CreateSpawner(Vector3 _direction)
+    {
+        Vector3 dpos = _direction * m_distance;
+        Vector3 position = transform.position + dpos;
+        GameObject spawner = (GameObject)Instantiate(m_asteroidSpawner, position, Quaternion.identity);
+        spawner.transform.SetParent(this.transform);
+        m_spawners.Add(spawner);
+    }
+}
